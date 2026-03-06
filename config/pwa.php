50dda866fa21d2634a82bf38567d6d9897b3291e
Omar karman <karmanomar9696@gmail.com>
2026-03-06 18:02:22 +0300

    Update PWA configuration and Livewire components
    This commit modifies the PWA configuration by changing the background color to black and updating the icon sources to new smaller and larger logo files. Additionally, it sets the debug mode to true for development purposes. The Livewire components have been enhanced with transaction management features, improving the handling of queued jobs and event listeners. The version of Livewire has also been updated to 3.15.8, ensuring compatibility with the latest features.

--- config/pwa.php
+++ config/pwa.php
@@ -34,12 +34,12 @@ return [
         'start_url' => '/',
         'icons' => [
             [
-                'src' => 'logo.png',
+                'src' => 'logo_sm.png',
                 'sizes' => '192x192',
                 'type' => 'image/png',
             ],
             [
-                'src' => 'logo.png',
+                'src' => 'log_lg.png',
                 'sizes' => '512x512',
                 'type' => 'image/png',
             ],
