d39043c0a0d798496e0f290e292499915efa6115
Omar karman <karmanomar9696@gmail.com>
2026-04-16 16:24:30 +0300

    testing the new design for the cricles slider

--- resources/views/pages/frontend/⚡main.blade.php
+++ resources/views/pages/frontend/⚡main.blade.php
@@ -27,7 +27,7 @@ new #[Layout('layouts::frontend')] class extends Component {
 };
 ?>
 
-<div class="flex flex-col sm:gap-8">
+<div class="flex flex-col">
 
     <!-- Start Circular Slider -->
     <section class="mx-auto w-full max-w-7xl" data-section="homepage-marquee" aria-labelledby="homepage-marquee-heading">
